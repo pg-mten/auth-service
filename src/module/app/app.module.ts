fb3bc76b0a362e40fa67cddc1783bb2342ebb2af
rucci <d.rucci.2001@gmail.com>
2025-06-20 08:55:56 +0700

    Prisma init

--- src/module/app/app.module.ts
+++ src/module/app/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CustomPrismaModule } from 'nestjs-prisma';
+import { ExtendedPrismaConfigService } from '../prisma/prisma-config.extension';
 
 @Module({
-  imports: [],
+  imports: [
+    CustomPrismaModule.forRootAsync({
+      isGlobal: true,
+      name: 'PrismaService',
+      useClass: ExtendedPrismaConfigService,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
