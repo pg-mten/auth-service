e83de539500b1d5e5f8f66318a3ae785a499601a
admin <admin@admins-MacBook-Pro.local>
2025-07-04 05:40:07 +0700

    fixing roles controller

--- src/module/roles/roles.controller.ts
+++ src/module/roles/roles.controller.ts
@@ -10,7 +10,9 @@ import {
 import { RolesService } from './roles.service';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
-import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { CheckPolicies } from '../casl/policy.decorator';
+import { AppAbility } from '../casl/casl-ability.factory';
 
 @ApiBearerAuth()
 @ApiTags('Roles')
@@ -19,28 +21,32 @@ export class RolesController {
   constructor(private readonly rolesService: RolesService) {}
 
   @Post()
+  @CheckPolicies((ability: AppAbility) => ability.can('create', 'Role'))
   create(@Body() dto: CreateRoleDto) {
     return this.rolesService.create(dto);
   }
 
   @Get()
-  @ApiOperation({ summary: 'Get all articles by current user' })
+  @CheckPolicies((ability: AppAbility) => ability.can('read', 'Role'))
   findAll() {
     console.log('controller');
     return this.rolesService.findAll();
   }
 
   @Get('/:id')
+  @CheckPolicies((ability: AppAbility) => ability.can('read', 'Role'))
   findOne(@Param('id') id: string) {
     return this.rolesService.findOne(+id);
   }
 
   @Put(':id')
+  @CheckPolicies((ability: AppAbility) => ability.can('update', 'Role'))
   update(@Param('id') id: string, @Body() dto: UpdateRoleDto) {
     return this.rolesService.update(+id, dto);
   }
 
   @Delete(':id')
+  @CheckPolicies((ability: AppAbility) => ability.can('delete', 'Role'))
   remove(@Param('id') id: string) {
     return this.rolesService.remove(+id);
   }
