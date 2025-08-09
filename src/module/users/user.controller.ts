c24667c11c522ca739ee49f0a9f7e1e98355c1ac
rucci <d.rucci.2001@gmail.com>
2025-08-09 22:29:37 +0700

    User Profile

--- src/module/users/user.controller.ts
+++ src/module/users/user.controller.ts
@@ -5,7 +5,6 @@ import { AuthInfoDto } from '../auth/dto/auth.dto';
 import { ApiBearerAuth, ApiOkResponse, ApiOperation } from '@nestjs/swagger';
 import { Roles } from '../auth/decorator/roles.decorator';
 import { Role } from 'src/shared/constant/auth.constant';
-import { Public } from '../auth/decorator/public.decorator';
 import { ProfileDto } from './dto/profile.dto';
 
 @Controller()
@@ -13,19 +12,12 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get('/user/profile')
-  @Public()
-  // @ApiBearerAuth()
+  @ApiBearerAuth()
   @ApiOperation({ summary: 'Profile' })
   @ApiOkResponse({ type: ProfileDto })
-  profile(@CurrentUser() user: AuthInfoDto) {
-    console.log({ user });
-    return this.userService.profile({
-      email: 'aaa',
-      id: 4,
-      firstName: 'a',
-      lastName: 'a',
-      role: Role.ADMIN_MERCHANT,
-    });
+  profile(@CurrentUser() authInfo: AuthInfoDto) {
+    console.log({ authInfo });
+    return this.userService.profile(authInfo);
   }
 
   @ApiBearerAuth()
