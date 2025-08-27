5463f887d54048397cecceca96d584b6da089ff3
rucci <d.rucci.2001@gmail.com>
2025-08-27 21:36:18 +0700

    policies

--- src/module/agent-detail/agent-detail.controller.ts
+++ src/module/agent-detail/agent-detail.controller.ts
@@ -32,14 +32,14 @@ export class AgentDetailController {
   constructor(private readonly service: AgentDetailService) {}
 
   @Post()
-  @CheckPolicies((ability: AppAbility) => ability.can('create', 'AgentDetail'))
+  // @CheckPolicies((ability: AppAbility) => ability.can('create', 'AgentDetail'))
   create(@Body() dto: CreateAgentDetailDto, @CurrentUser('id') userId: number) {
     return this.service.create(userId, dto);
   }
 
   // TODO Pagination
   @Get()
-  @CheckPolicies((ability: AppAbility) => ability.can('read', 'AgentDetail'))
+  // @CheckPolicies((ability: AppAbility) => ability.can('read', 'AgentDetail'))
   @ApiOperation({ summary: 'List of Agents' })
   @ApiOkResponse({ type: AgentDto, isArray: true })
   findAll() {
@@ -55,7 +55,7 @@ export class AgentDetailController {
   }
 
   @Get(':id')
-  @CheckPolicies((ability: AppAbility) => ability.can('read', 'AgentDetail'))
+  // @CheckPolicies((ability: AppAbility) => ability.can('read', 'AgentDetail'))
   @ApiOperation({ summary: 'Agent by id' })
   @ApiOkResponse({ type: AgentDto })
   findOne(@Param('id', ParseIntPipe) id: number) {
@@ -63,7 +63,7 @@ export class AgentDetailController {
   }
 
   @Patch('update/:id')
-  @CheckPolicies((ability: AppAbility) => ability.can('update', 'AgentDetail'))
+  // @CheckPolicies((ability: AppAbility) => ability.can('update', 'AgentDetail'))
   @ApiOperation({ summary: 'Update Agent by id' })
   @ApiBody({ type: UpdateAgentDetailDto })
   async update(
