8299bac9f02f5ae019bec68c3574d8e3ef0d7b3c
rucci <d.rucci.2001@gmail.com>
2026-02-04 17:08:05 +0700

    fix

--- src/modules/merchant-signature/merchant-signature.controller.ts
+++ src/modules/merchant-signature/merchant-signature.controller.ts
@@ -1,21 +1,15 @@
-import {
-  Controller,
-  Get,
-  Query,
-  UseInterceptors,
-  UsePipes,
-} from '@nestjs/common';
+import { Controller, Get, Query, UseInterceptors } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { MerchantSignatureService } from './merchant-signature.service';
 import { AuthInfoDto } from 'src/microservice/auth/dto/auth-info.dto';
 import { CurrentAuthInfo } from 'src/microservice/auth/decorator/current-auth-info.decorator';
-import { FilterMerchantValidateSignatureSystemDto } from 'src/microservice/auth/dto-system/filter-merchant-validate-signature.system.dto';
 import { SERVICES } from 'src/shared/constant/client.constant';
 import { ResponseInterceptor } from 'src/shared/interceptor';
 import { CustomValidationPipe } from 'src/shared/pipe';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { SystemApi } from 'src/microservice/auth/decorator/system.decorator';
 import { MerchantApi } from 'src/microservice/auth/decorator/merchant.decorator';
+import { FilterMerchantSignatureValidationSystemDto } from 'src/microservice/merchant-signature/filter-merchant-signature-validation.system.dto';
 
 @ApiTags('Merchant Signature')
 @Controller('merchant-signature')
@@ -43,15 +37,17 @@ export class MerchantSignatureController {
 
   @SystemApi()
   @Get('/internal/validate-signature')
-  validateSignature(@Query() filter: FilterMerchantValidateSignatureSystemDto) {
+  validateSignature(
+    @Query() filter: FilterMerchantSignatureValidationSystemDto,
+  ) {
     return this.service.validateSignature(filter);
   }
 
-  @MessagePattern({ cmd: SERVICES.AUTH.cmd.merchant_validate_signature })
+  @MessagePattern({ cmd: SERVICES.AUTH.cmd.merchant_signature_validation })
   @UseInterceptors(ResponseInterceptor)
   validateSignatureTCP(
     @Payload(CustomValidationPipe)
-    payload: FilterMerchantValidateSignatureSystemDto,
+    payload: FilterMerchantSignatureValidationSystemDto,
   ) {
     return this.service.validateSignature(payload);
   }
